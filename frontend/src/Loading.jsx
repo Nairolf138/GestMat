ce53f9da310a4101ea9382ffe507227142a4eec1
Nairolf138 <71496832+Nairolf138@users.noreply.github.com>
2025-09-02 17:06:25 +0200

    feat: translate loading component

--- frontend/src/Loading.jsx
+++ frontend/src/Loading.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import { useTranslation } from 'react-i18next';
 
 function Loading() {
+  const { t } = useTranslation();
   return (
     <div className="d-flex justify-content-center py-3">
       <div className="spinner-border" role="status">
-        <span className="visually-hidden">Loading...</span>
+        <span className="visually-hidden">{t('common.loading')}</span>
       </div>
     </div>
   );
