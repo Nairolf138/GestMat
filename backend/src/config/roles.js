1397cceb9ca46f1e8497d4102d1211c14252ac34
Nairolf138 <71496832+Nairolf138@users.noreply.github.com>
2025-09-12 19:04:29 +0200

    Replace manager roles with regisseur roles

--- backend/src/config/roles.js
+++ backend/src/config/roles.js
@@ -1,25 +1,23 @@
 const ADMIN_ROLE = 'Administrateur';
-const EQUIPMENT_MANAGER_ROLE = 'Gestionnaire Equipement';
-const LOAN_MANAGER_ROLE = 'Gestionnaire Pret';
-const USER_MANAGER_ROLE = 'Gestionnaire Utilisateurs';
-const STRUCTURE_MANAGER_ROLE = 'Gestionnaire Structures';
+const REGISSEUR_GENERAL_ROLE = 'Regisseur General';
+const REGISSEUR_LUMIERE_ROLE = 'Regisseur Lumiere';
+const REGISSEUR_SON_ROLE = 'Regisseur Son';
+const REGISSEUR_PLATEAU_ROLE = 'Regisseur Plateau';
+const AUTRE_ROLE = 'Autre';
 
 const ROLES = [
   ADMIN_ROLE,
-  EQUIPMENT_MANAGER_ROLE,
-  LOAN_MANAGER_ROLE,
-  USER_MANAGER_ROLE,
-  STRUCTURE_MANAGER_ROLE,
-  'Regisseur Son',
-  'Regisseur Lumiere',
-  'Regisseur Plateau',
-  'Regisseur General',
-  'Autre',
+  REGISSEUR_SON_ROLE,
+  REGISSEUR_LUMIERE_ROLE,
+  REGISSEUR_PLATEAU_ROLE,
+  REGISSEUR_GENERAL_ROLE,
+  AUTRE_ROLE,
 ];
 
 module.exports = ROLES;
 module.exports.ADMIN_ROLE = ADMIN_ROLE;
-module.exports.EQUIPMENT_MANAGER_ROLE = EQUIPMENT_MANAGER_ROLE;
-module.exports.LOAN_MANAGER_ROLE = LOAN_MANAGER_ROLE;
-module.exports.USER_MANAGER_ROLE = USER_MANAGER_ROLE;
-module.exports.STRUCTURE_MANAGER_ROLE = STRUCTURE_MANAGER_ROLE;
+module.exports.REGISSEUR_GENERAL_ROLE = REGISSEUR_GENERAL_ROLE;
+module.exports.REGISSEUR_LUMIERE_ROLE = REGISSEUR_LUMIERE_ROLE;
+module.exports.REGISSEUR_SON_ROLE = REGISSEUR_SON_ROLE;
+module.exports.REGISSEUR_PLATEAU_ROLE = REGISSEUR_PLATEAU_ROLE;
+module.exports.AUTRE_ROLE = AUTRE_ROLE;
